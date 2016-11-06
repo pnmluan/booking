201f1cd8d8da8b298fbc7eee3be2d0740d3d6f51
dung <dung>
2016-11-07 02:15:51 +0700

    update crawler vietjet

--- routes/web.php
+++ routes/web.php
@@ -53,4 +53,7 @@ $app->group(['prefix' => 'api/v1', 'middleware' => 'BasicAuth', 'namespace' => '
 {
     $app->get('airline','TicketCrawlers\AirlineController@index');
 
+    $app->get('airline/test','TicketCrawlers\AirlineController@test');
+    $app->post('airline/vietjet','TicketCrawlers\AirlineController@vietjet');
+
 });
