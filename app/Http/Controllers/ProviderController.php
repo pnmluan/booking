b20f80f3ee419139919900f661d39d68cd2e19db
banamlehsb <09910122hsb>
2016-11-25 00:00:36 +0700

    update provider api

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -10,43 +10,79 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Provider;
+use Illuminate\Http\Request;
 
 class ProviderController extends ApiController
 {
-    public function index(){
-        $providers  = Provider::all();
+    public function index(Request $request){
 
-        return response()->json($providers);
+        $data = Provider::listItems($request->all());
+        return response()->json($data);
 
     }
 
-    public function show($id){
+    public function show($id) {
 
         $provider  = Provider::find($id);
 
-        return response()->json($provider);
+        if (!$provider) {
+            return $this->respondNotFound();
+        }
+        return $this->respondWithSuccess(['data'=>$provider]);
     }
 
     public function create(Request $request){
+        $provider = new Provider();
+        $data = $request['data'];
 
-        $provider = Provider::create($request->all());
+        $provider->fill($data);
 
-        return response()->json($provider);
-
+        if (!$provider->isValid()) {
+            return $this->respondWithError(['error' => $provider->getValidationErrors()]);
+        }
+        try {
+            $provider->save();
+        } catch (\Exception $ex) {
+            return $this->respondWithNotSaved();
+        }
+        return $this->respondWithCreated(['data'=>$provider]);
     }
 
     public function delete($id){
-        $provider  = Provider::find($id);
-        $provider->delete();
 
-        return response()->json('deleted');
+        $provider  = Provider::find($id);
+        if (!$provider) {
+            return $this->respondNotFound();
+        }
+        try {
+            if (!$provider->delete()) {
+                return $this->respondWithError();
+            }
+        } catch (\Exception $ex) {
+            return $this->respondWithError(['error' => $ex->getMessage()]);
+        }
+        return $this->respondWithSuccess(['record_id'=>$id]);
     }
 
-    public function update(Request $request,$id){
-        $provider  = Provider::find($id);
-        $provider->name = $request->input('name');
-        $provider->save();
+    public function update(Request $request, $id){
 
-        return response()->json($provider);
+        $provider = Provider::find($id);
+        if(!$provider) {
+            return $this->respondNotFound();
+        }
+        $data = $request['data'];
+
+        $provider->fill($data);
+
+        if (!$provider->isValid()) {
+            return $this->respondWithError(['error' => $provider->getValidationErrors()]);
+        }
+        try {
+            $provider->save();
+        } catch (\Exception $ex) {
+            return $this->respondWithNotSaved();
+        }
+
+        return $this->respondWithSaved(['data'=>$provider]);
     }
 }
