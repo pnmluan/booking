7b242b2abe2b2de1ede8a28a4b658504f08ee6b2
Luan <l_phan@vision-net.co.jp>
2017-01-11 16:59:48 +0700

    Add warning msg validate

--- public/angular2/src/app/components/home/home.component.ts
+++ public/angular2/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import {
 	FormGroup,
@@ -10,6 +10,7 @@ import {
 import { LocalStorageService } from 'angular-2-local-storage';
 import { SelectModule } from 'angular2-select';
 import { UUID } from 'angular2-uuid';
+import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
 
 import { BannerComponent } from './banner';
 import { NewsComponent } from './news';
@@ -28,6 +29,8 @@ declare let moment: any;
   providers: [BannerComponent, NewsComponent, BannerComponent, LocationDataService, BannerDataService, NewsDataService]
 })
 export class HomeComponent implements OnInit, AfterViewInit {
+	@ViewChild('warning') warning: ModalComponent;
+	warningMsg = '';
 	people = this._Configuration.arr_number_people;
 	infants = this._Configuration.arr_number_infants;
 	locations = [];
@@ -111,31 +114,51 @@ export class HomeComponent implements OnInit, AfterViewInit {
 	}
 	
 	onSubmit() {
-		let uuid = UUID.UUID();
-		this.sessionStorage.remove('session_flight');
-		this.sessionStorage.remove('session_token');
-		this.sessionStorage.set('session_token', uuid);
+
 
 		var objectStore = this.filterBookForm.value;
+
+		if (objectStore.from == objectStore.to) {
+			this.warningMsg = 'Điểm đi và Điểm đến không được trùng nhau.';
+			this.warning.open();
+			return;
+		}
+		objectStore.from_date = moment(objectStore.from_date, this._Configuration.viFormatDate).format(this._Configuration.dateFormat);
+		if (objectStore.from_date < moment().format(this._Configuration.dateFormat)) {
+			this.warningMsg = 'Ngày đi phải nhỏ hơn ngày hiện tại.';
+			this.warning.open('sm');
+			return;
+		}
+		
 		objectStore.adult = jQuery('.select-adult').val();
 		objectStore.children = jQuery('.select-child-1').val();
 		objectStore.infant = jQuery('.select-child-2').val();
-		var dateFormat = 'YYYY-MM-DD';
-		var viFormatDate = 'DD/MM/YYYY';
-		objectStore.from_date = moment(objectStore.from_date, viFormatDate).format(dateFormat);
+
+		
 		objectStore.from_name = this.getNameFromCode(objectStore.from);
 		objectStore.to_name = this.getNameFromCode(objectStore.to);
 
 		if (objectStore.to_date == undefined) {
 			objectStore.to_date = '';
-			
+
 		} else {
-			objectStore.to_date = moment(objectStore.to_date, viFormatDate).format(dateFormat);
+			objectStore.to_date = moment(objectStore.to_date, this._Configuration.viFormatDate).format(this._Configuration.dateFormat);
+			if (objectStore.from_date > objectStore.to_date) {
+				this.warningMsg = 'Ngày đi phải nhỏ hơn Ngày đến.';
+				this.warning.open();
+				return;
+			}
 			objectStore.to_name = this.getNameFromCode(objectStore.to);
 		}
 
+		let uuid = UUID.UUID();
+		this.sessionStorage.remove('session_flight');
+		this.sessionStorage.remove('session_token');
+		this.sessionStorage.set('session_token', uuid);
 		this.sessionStorage.set('session_flight', JSON.stringify(objectStore));
 		this.router.navigate(['search-result/' + uuid]);
+		
+		
 	}
 
 	selectPlaneOption(round_trip) {
