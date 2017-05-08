5c95b4e7312b49b3f3d414098f960ae67434b2bd
Jay Lu <ta.pnmluan@gmail.com>
2017-05-08 23:29:06 +0700

    Add policy + tutorial scroll

--- public/angular2/src/app/components/tutorial/tutorial.component.ts
+++ public/angular2/src/app/components/tutorial/tutorial.component.ts
@@ -3,6 +3,7 @@ import { Title } from '@angular/platform-browser';
 import { NgForm } from '@angular/forms';
 import { URLSearchParams } from '@angular/http';
 import { ToasterService } from 'angular2-toaster/angular2-toaster';
+declare let jQuery: any;
 
 @Component({
 	selector: 'app-tutorial',
@@ -19,6 +20,21 @@ export class TutorialComponent implements OnInit {
 
 	ngOnInit() {
 		this._title.setTitle('Hướng dẫn | Datvesieure');
+
+		setTimeout(()=> {
+			jQuery("a[href^=#]").click(function(e) {
+				e.preventDefault();
+				var dest = jQuery(this).attr('href');
+				console.log(dest);
+				jQuery('html,body').animate({ scrollTop: jQuery(dest).offset().top - 120 }, 'fast');
+			});
+
+			jQuery('.in-this-article').stick_in_parent();
+
+			jQuery(".select-from, .select-to").select2({
+				width: '100%'
+			});
+		}, 100);
 	}
 
 }
