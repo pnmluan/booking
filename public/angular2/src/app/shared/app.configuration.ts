60b6ccf22d11459c167bb44ae24357988deb41ea
Luan <l_phan@vision-net.co.jp>
2017-08-04 17:06:01 +0700

    Add config bank_account

--- public/angular2/src/app/shared/app.configuration.ts
+++ public/angular2/src/app/shared/app.configuration.ts
@@ -16,9 +16,29 @@ export class Configuration {
 	public formatDate = 'YYYY-MM-DD';
 	public longDateTime = 'YYYY-MM-DD HH:mm:ss';
 	public longFormatDateTime = "HH:mm-dddd-DD/MM/YYYY";
-	public session_expired = 30/60; //Session 15 mins
+	public session_expired = 30 / 60; //Session 15 mins
 
 	public arr_number_people = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-	public arr_number_infants = ['1','2'];
+	public arr_number_infants = ['1', '2'];
 	public number_order: number = 0;
+
+
+	public bank_accounts = {
+		vietcombank: {
+			icon: 'assets/img/bank/bank-logo-VCB.jpg',
+			number_account: '0451000328295',
+			branch: 'Vietcombank chi nhánh Thành Công, Hà Nội'
+		},
+		bidv: {
+			icon: '',
+			number_account: '31310000341958',
+			branch: 'BIDV chi nhánh Bắc Sài Gòn, TPHCM'
+		},
+		techcombank: {
+			icon: '',
+			number_account: '19031034880011',
+			branch: 'Techcombank chi nhánh Hà Nội'
+		}
+	};
+
 }
